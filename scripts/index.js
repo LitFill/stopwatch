05ecceded58ff9707a8d529a64f4bb15d7bd2f23
LitFill <98679748+LitFill@users.noreply.github.com>
2023-07-27 22:36:15 +0700

    convert all functions to named functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,58 +1,59 @@
-const timeElement = document.getElementById('time');
-const lapTimeElement = document.getElementById('lap');
-const leftButton = document.getElementById('btnLeft');
-const rightButton = document.getElementById('btnRight');
+const timeElement = document.getElementById("time");
+const lapTimeElement = document.getElementById("lap");
+const leftButton = document.getElementById("btnLeft");
+const rightButton = document.getElementById("btnRight");
 
 const lapTimes = [];
 let elapsedTime = 0;
 let interval;
 let isCounting = false;
 
-const formatTime = time => {
+function formatTime(time) {
     const minutes = Math.floor(time / (60 * 100));
     const seconds = Math.floor((time / 100) % 60);
     const miliseconds = time % 100;
     return (
-        (minutes < 10 ? '0' + minutes: minutes)
-        + ':'
-        + (seconds < 10 ? '0' + seconds: seconds)
-        + '.'
-        + (miliseconds < 10 ? '0' + miliseconds: miliseconds)
+        (minutes < 10 ? "0" + minutes : minutes) +
+        ":" +
+        (seconds < 10 ? "0" + seconds : seconds) +
+        "." +
+        (miliseconds < 10 ? "0" + miliseconds : miliseconds)
     );
-};
+}
 
-const formatTimeShort = time => {
+function formatTimeShort(time) {
     const minutes = Math.floor(time / (60 * 100));
     const seconds = Math.floor((time / 100) % 60);
     const milliseconds = time % 100;
 
-    let formattedTime = '';
+    let formattedTime = "";
     if (minutes > 0) {
-        formattedTime += `${minutes < 10 ? '0' + minutes: minutes}:`;
+        formattedTime += `${minutes < 10 ? "0" + minutes : minutes}:`;
     }
-    formattedTime += `${seconds < 10 ? '0' + seconds: seconds}.${milliseconds < 10 ? '0' + milliseconds: milliseconds}`;
+    formattedTime += `${seconds < 10 ? "0" + seconds : seconds}.${
+        milliseconds < 10 ? "0" + milliseconds : milliseconds
+    }`;
 
     return formattedTime;
-};
+}
 
-
-const startCounting = () => {
+function startCounting() {
     if ((isCounting = !isCounting)) {
         interval = setInterval(() => {
             elapsedTime += 1;
             displayTime();
             console.log(elapsedTime);
         }, 10);
-        leftButton.innerText = 'pause';
-        rightButton.innerText = 'lap';
+        leftButton.innerText = "pause";
+        rightButton.innerText = "lap";
     } else {
         clearInterval(interval);
-        leftButton.innerText = 'start';
-        rightButton.innerText = 'reset';
+        leftButton.innerText = "start";
+        rightButton.innerText = "reset";
     }
-};
+}
 
-const resetCount = () => {
+function resetCount() {
     if (isCounting) {
         // lapTimes.push(formatTime(elapsedTime));
         lapTimes.push(elapsedTime);
@@ -61,12 +62,12 @@ const resetCount = () => {
         elapsedTime = 0;
         displayTime();
         lapTimes.splice(0, lapTimes.length);
-        lapTimeElement.innerText = 'lap time:';
+        lapTimeElement.innerText = "lap time:";
         // displayLapTimes();
     }
-};
+}
 
-const displayTime = () => {
+function displayTime() {
     timeElement.innerText = formatTime(elapsedTime);
     if (!elapsedTime) {
         rightButton.disabled = true;
@@ -77,8 +78,7 @@ const displayTime = () => {
         // rightButton.style.opacity = 1;
         // rightButton.style.cursor = 'pointer';
     }
-};
-
+}
 
 // const displayLapTimes = () => {
 //     let text = '';
@@ -90,22 +90,25 @@ const displayTime = () => {
 // };
 
 // const displayLapTimes = () => lapTimeElement.innerText = lapTimes.map((lapTime, index) => `${index + 1}. ${lapTime}`).join('\n');
-const displayLapTimes = () => {
-    let text = '';
+function displayLapTimes() {
+    let text = "";
     lapTimes.forEach((lapTime, index) => {
         if (index > 0) {
             const prevLapTime = lapTimes[index - 1];
             const lapTimeDiff = lapTime - prevLapTime;
-            text += `\n${index + 1}. ${formatTime(lapTime)} (+${formatTimeShort(lapTimeDiff)})`;
+            text += `\n${index + 1}. ${formatTime(lapTime)} (+${formatTimeShort(
+                lapTimeDiff
+            )})`;
         } else {
-            text += `\n${index + 1}. ${formatTime(lapTime)} (+${formatTimeShort(lapTime)})`
+            text += `\n${index + 1}. ${formatTime(lapTime)} (+${formatTimeShort(
+                lapTime
+            )})`;
         }
     });
     lapTimeElement.innerText = text;
-};
-
+}
 
 displayTime();
 
-leftButton.addEventListener('click', startCounting);
-rightButton.addEventListener('click', resetCount);
+leftButton.addEventListener("click", startCounting);
+rightButton.addEventListener("click", resetCount);
